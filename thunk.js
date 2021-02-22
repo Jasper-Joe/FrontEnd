27bf1bdb209623b2dc15146c450eecd835daffa5
jasper <jasperzhou1129@gmail.com>
2021-02-22 11:23:27 -0500

    thunk

---
+++ thunk.js
@@ -0,0 +1,14 @@
+
+function fetchData(fn) {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => fn(json))
+}
+
+const asyncThunk = function() {
+  return fetchData(function getData(data){
+    console.log(data)
+  })
+}
+
+asyncThunk()
